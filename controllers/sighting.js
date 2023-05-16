070aff437a405e09b21a1a364b2d4ed39c26c17a
Abra992 <102362026+Abra992@users.noreply.github.com>
2023-05-16 05:49:39 +0100

    make the path right for image

--- controllers/sighting.js
+++ controllers/sighting.js
@@ -6,7 +6,7 @@ exports.insert = function (req, res) {
     let sighting = new Sighting({
         poster_id: req.body.user_session_id,
         poster_name: req.body.username,
-        img: req.file.path,
+        img: `/uploads/${req.file.filename}`,
         description: req.body.description,
         identification: req.body.identification,
     });
