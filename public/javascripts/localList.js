6ccf45784de3be48d5b5f3d2b22f0d5dfa627941
Ross Vickers <wossyrus@gmail.com>
2023-05-19 05:58:08 +0100

    added explanation to storage idea

--- public/javascripts/localList.js
+++ public/javascripts/localList.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 const updated = null
 //true false to check if it was online last time page was accessed
-let total = 0
-//value 0 -> 20 | counts number of files in memory
 
 let item = {
-    image: "missing",
+    image: "missing.jpg",
     date: 15,
     id: "bird_name",
     name: "Person"
@@ -17,7 +15,18 @@ storeItem = function (itemNum, item) {
     localStorage.setItem(itemNum, JSON.stringify(item))
 }
 
-getItem = function (itemNum){
+pullItem = function (itemNum){
     const data = localStorage.getItem(itemNum)
     item = JSON.parse(data)
 }
+
+/*
+basic premise is to take basic info from the database and convert it into an "item"
+this item is stored as a valued JSON.
+If the user is not connected to the DB, the items should be pulled and read instead
+Local storage apparently is able to work across pages within a html, so might work to add values when offline too
+also lets us store and pull across different files
+
+localstorage.removeItem(X) removes item with key X from storage
+localstorage.clear() wipes all values stored within the storage
+ */
